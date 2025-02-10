f333b93cdb1b96de6ae9d4f535bf6becf294075e
Paulo Cesar <paulocgsantos99@gmail.com>
2025-02-10 18:53:27 -0300

    feat: add post e put add exception errors message

--- tasks-fullstack/src/main/java/com/paulotech/tasks_fullstack/controller/TaskListController.java
+++ tasks-fullstack/src/main/java/com/paulotech/tasks_fullstack/controller/TaskListController.java
@@ -1,18 +1,20 @@
 package com.paulotech.tasks_fullstack.controller;
 
 import com.paulotech.tasks_fullstack.domain.dto.TaskListDto;
+import com.paulotech.tasks_fullstack.domain.entities.TaskList;
 import com.paulotech.tasks_fullstack.mappers.TaskListMapper;
 import com.paulotech.tasks_fullstack.service.TaskListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/task-lists")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskListController {
 
     private final TaskListService taskListService;
@@ -23,6 +25,30 @@ public class TaskListController {
         return taskListService.listTaskLists().stream()
                 .map(taskListMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto){
+        TaskList createdTaskList = taskListService.createTaskList(
+                taskListMapper.fromDto(taskListDto)
+        );
+        return taskListMapper.toDto(createdTaskList);
+    }
+
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id")UUID taskListId){
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
+    }
+
+    @PutMapping(path = "/{task_list_id}")
+    public TaskListDto updateTaskList(@PathVariable("task_list_id")UUID taskListId,
+                                      @RequestBody TaskListDto taskListDto
+    ){
+        TaskList updatedTaskList = taskListService.updateTaskList(
+                taskListId,
+                taskListMapper.fromDto(taskListDto)
+        );
+        return taskListMapper.toDto(updatedTaskList);
     }
 
 }
